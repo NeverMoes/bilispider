c831fe7e0bf0c1fcb9e5440ad78dad18de8b5437
NeverMoes <nevermoes@gmail.com>
2017-01-08 19:40:12 +0800

    test

--- spider.py
+++ spider.py
@@ -1,5 +1,32 @@
 import requests
+import re
+import json
 from consts import const
 
-req = requests.get(const.API_STAT)
+req = requests.get(const.API_STAT_FORMAT.format(av=6536710))
+response = req.content.decode('utf')
+
+
+def parser(content):
+    pat_api_stat = re.compile(r'\((.+)\)')
+    jsonstr = pat_api_stat.findall(content)[0]
+    jsondic = json.loads(jsonstr)
+    return jsondic
+
+
+data = parser(response)
+
+print(data)
+
 print(req.content)
+
+
+# class VideoInfo(object):
+#     def __init__(self, av):
+#         self.av = av
+#
+#
+#
+#     @staticmethod
+#     def test_api():
+#         pass
